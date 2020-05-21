589a550ce2d161d5d742ec441555c6f7b6d1593b
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-05-21 08:43:40 +0100

    Do not show skippable food questions if not answered
    We should not be showing questions on the check your answers page where
    users have not answered the question.

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,4 +1,6 @@
 module AnswersHelper
+  SKIPPABLE_QUESTIONS = %w[carry_supplies dietary_requirements medical_conditions nhs_number].freeze
+
   def answer_items
     answers = questions.map do |question|
       answer = concat_answer(session[question], question)
@@ -17,7 +19,7 @@ module AnswersHelper
   end
 
   def skip_question?(question, answer)
-    question.in?(%w[nhs_number medical_conditions]) && answer.nil?
+    question.in?(SKIPPABLE_QUESTIONS) && answer.nil?
   end
 
   def concat_answer(answer, question)
